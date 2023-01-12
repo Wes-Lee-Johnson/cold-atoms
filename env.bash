f2c96009fbb7ffaf6616ca7cc8cae039e0caed54
Wes-Lee-Johnson <quantumcat.wj@gmail.com>
2023-01-12 13:47:08 -0700

    removed env.bash

--- env.bash
+++ env.bash
@@ -1,11 +1,10 @@
-!/bin/bash
+#!/bin/bash
 
 
 #this bash script contains commands to make envirnment 
 #jan 12 2023, Wes Johnson 
 
 
-
 conda create -n env_py3.7_git python=3.7 
-conda activate env_py3.7_git
+#conda activate env_py3.7_git
 
